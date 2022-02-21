5500bf1658fc5a52929e48628647dc6cdd3f06b1
Hleb Sharstuk <sharhleb@gmail.com>
2022-02-21 15:28:41 +0300

    feat(AH06): add routerLinkActive

--- src/app/user-info/user-info-routing.module.ts
+++ src/app/user-info/user-info-routing.module.ts
@@ -7,15 +7,15 @@ import { UserPowerUpsComponent } from './user-power-ups/user-power-ups.component
 import { UserHistoryComponent } from './user-history/user-history.component';
 
 import { ROUTE_CONFIGS } from '../utils/const/routes.consts';
+import { emptyString } from '../utils/const/validators.const';
 
 const routes: Routes = [
-  {path: '', component: UserLayoutComponent, children: [
+  {path: emptyString, component: UserLayoutComponent, children: [
       {path: ROUTE_CONFIGS.root.path, redirectTo: ROUTE_CONFIGS.userHeroList.path, pathMatch: 'full'},
       {path: ROUTE_CONFIGS.userHeroList.path, component: UserHeroListComponent},
       {path: ROUTE_CONFIGS.userHistory.path, component: UserHistoryComponent},
       {path: ROUTE_CONFIGS.userPowerUps.path, component: UserPowerUpsComponent},
     ]},
-
 ];
 
 @NgModule({
